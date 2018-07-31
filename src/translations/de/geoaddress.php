98bcfe912a6b8b4869a90f97cbafd79c389ea963
Udo Leiteritz <udo@udoleiteritz.com>
2018-07-31 14:36:38 +0200

    Added German translation

---
+++ src/translations/de/geoaddress.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+	'Address' => 'Adresse',
+	'Zipcode' => 'Postleitzahl',
+	'City' => 'Stadt',
+    'Country' => 'Land',
+	'No coordinates found yet' => 'Noch keine Koordinaten gefunden'
+];
